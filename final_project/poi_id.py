d6c1621f44880e599fb3bed88857316dbc02ca10
GabrielSGoncalves <gabriel@codenomics.cloud>
2019-05-13 10:19:37 -0300

    add reference file and organize poi_id.py

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import pickle
@@ -17,6 +17,7 @@ from tester import dump_classifier_and_data
 # not contributing much for the model:
 #                   loan_advances, director_fees, restricted_stock_deferred
 # So we decided to remove these features from the analysis.
+np.random.seed(1234)
 
 features_list = ['deferred_income',
                  'expenses',
@@ -58,7 +59,6 @@ df_enron = df_enron.fillna(0.0)
 # Rescaling feature values with MinMaxScaler
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-# df_features_no_outliers = df_features.drop('TOTAL')
 X = scaler.fit_transform(df_enron.drop('poi', axis=1))
 y = df_enron.poi
 
@@ -75,15 +75,17 @@ outlier_detection = DBSCAN(
     n_jobs=-1)
 clusters = outlier_detection.fit_predict(X)
 df_dbscan = pd.DataFrame(clusters, index=df_enron.index)
-print(df_dbscan[df_dbscan[0] == -1].index)
+#print(df_dbscan[df_dbscan[0] == -1].index)
 
+list_dbscan_outliers = list(df_dbscan[df_dbscan[0] == -1].index)
+print(list_dbscan_outliers)
 
 # As mentioned on the report, we applied PCA to define outliers and ended
 # finding one sample called "TOTAL" as a candidate outlier
 from sklearn.decomposition import PCA
-pca = PCA(n_components=2)
+pca = PCA(n_components=2, random_state=42)
 dim_red = pca.fit_transform(X)
-print(pca.singular_values_)
+# print(pca.singular_values_)
 
 # Transform PCA results into Dataframe
 pca_df = pd.DataFrame(dim_red, columns=[
@@ -98,30 +100,30 @@ pca_df['PCA2_zscore'] = (pca_df.PCA2 - pca_df.PCA2.mean()) / pca_df.PCA2.std()
 
 
 # Get rows that are 3 standard deviations away from mean
-list_outliers = (pca_df[(np.abs(pca_df.PCA1_zscore) > 3)
-                        | (np.abs(pca_df.PCA2_zscore) > 3)]).index
+list_outliers_pca = (pca_df[(np.abs(pca_df.PCA1_zscore) > 3)
+                            | (np.abs(pca_df.PCA2_zscore) > 3)]).index
 
-print(list_outliers)
+# print(list_outliers_pca)
 
-
+"""
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-df_features_no_outliers = df_enron.drop(list_outliers)
+df_features_no_outliers = df_enron.drop(list_outliers_pca)
 X = scaler.fit_transform(df_features_no_outliers.drop('poi', axis=1))
 y = df_features_no_outliers.poi
-
+"""
 
 # Spliting data into trainning and testing
 from sklearn.model_selection import train_test_split
 # Split your data X and y, into a training and a test set and fit the
 # pipeline onto the training data
-# X_train, X_test, y_train, y_test = train_test_split(
+# X_train, X_test, y_train, y_test = train_test_split(None
 #    X_resampled,  y_resampled, test_size=0.3,
 #    random_state=0)
 
 from imblearn.over_sampling import SMOTE
 # Define the resampling method
-method = SMOTE(kind='regular')
+method = SMOTE(kind='regular', random_state=42)
 
 # Create the resampled feature set
 X_resampled, y_resampled = method.fit_sample(X, y)
@@ -138,7 +140,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_resampled,  y_resampled, test_size=0.5, random_state=0)
 
 
-print(df_enron)
+# print(df_enron)
 # Task 3: Create new feature(s)
 # Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -163,8 +165,9 @@ clf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini'
                              min_impurity_decrease=0.0, min_impurity_split=None,
                              min_samples_leaf=1, min_samples_split=2,
                              min_weight_fraction_leaf=0.0, n_estimators='warn', n_jobs=None,
-                             oob_score=False, random_state=None, verbose=0,
+                             oob_score=False, random_state=42, verbose=0,
                              warm_start=False)
+print(clf)
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
 probs = clf.predict_proba(X_test)
@@ -182,12 +185,31 @@ print(confusion_matrix(y_test, predicted))
 # function. Because of the small size of the dataset, the script uses
 # stratified shuffle split cross validation. For more info:
 # http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+from sklearn.model_selection import StratifiedShuffleSplit
+"""
+sss = StratifiedShuffleSplit(n_splits=5, test_size=0.5, random_state=0)
+for train_index, test_index in sss.split(X_resampled, y_resampled):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.model_selection import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+    clf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+                                 max_depth=None, max_features='auto', max_leaf_nodes=None,
+                                 min_impurity_decrease=0.0, min_impurity_split=None,
+                                 min_samples_leaf=1, min_samples_split=2,
+                                 min_weight_fraction_leaf=0.0, n_estimators='warn', n_jobs=None,
+                                 oob_score=False, random_state=None, verbose=0,
+                                 warm_start=False)
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test)
+    probs = clf.predict_proba(X_test)
 
+    # Print the ROC curve, classification report and confusion matrix
+    from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
+    print(roc_auc_score(y_test, probs[:, 1]))
+    print(classification_report(y_test, predicted))
+    print(confusion_matrix(y_test, predicted))
+"""
 # Task 6: Dump your classifier, dataset, and features_list so anyone can
 # check your results. You do not need to change anything below, but make sure
 # that the version of poi_id.py that you submit can be run on its own and
